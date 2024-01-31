677b31232b1d09e320aec0a0c511962684b09921
Bunyod0301 <bunyodyusupov0@gmail.com>
2024-01-31 22:31:35 +0500

    import ui library css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';  
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-dark-indigo/theme.css';
+import { PrimeReactProvider } from "primereact/api";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PrimeReactProvider>
+        {children}
+        </PrimeReactProvider>
+      </body>
     </html>
   );
 }
