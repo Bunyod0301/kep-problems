11cda60bae65948c4a840a6e255561831efda30f
Bunyod0301 <bunyodyusupov0@gmail.com>
2024-02-02 01:06:29 +0500

    fix error

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DataGrid from "@/Components/DataTable";
+import { Suspense } from 'react';
 import "./globals.css";
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';  
@@ -9,10 +10,13 @@ import { PrimeReactProvider } from "primereact/api";
 import { SkeletonTheme } from "react-loading-skeleton";
 export default function Home() {
   return (
+    
+    <Suspense fallback={<div>Loading...</div>}>
     <SkeletonTheme baseColor="#202020" highlightColor="#444">
       <PrimeReactProvider>
         <DataGrid />
       </PrimeReactProvider>
     </SkeletonTheme>
+  </Suspense>
   );
 }
